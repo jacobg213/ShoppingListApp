df77e57e7bee93511623b379094255bd86e67e5b
Jacob Gilis <gilis.jakub@gmail.com>
2017-03-01 17:09:25 +0100

    Create the project

---
+++ app/src/main/java/org/projects/shoppinglist/MainActivity.java
@@ -0,0 +1,88 @@
+package org.projects.shoppinglist;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    ArrayAdapter<String> adapter;
+    ListView listView;
+    ArrayList<String> bag = new ArrayList<String>();
+
+    public ArrayAdapter getMyAdapter()
+    {
+        return adapter;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        //getting our listiew - you can check the ID in the xml to see that it
+        //is indeed specified as "list"
+        listView = (ListView) findViewById(R.id.list);
+        //here we create a new adapter linking the bag and the
+        //listview
+        adapter =  new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_checked,bag );
+
+        //setting the adapter on the listview
+        listView.setAdapter(adapter);
+        //here we set the choice mode - meaning in this case we can
+        //only select one item at a time.
+        listView.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
+
+        Button addButton = (Button) findViewById(R.id.addButton);
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText nameInput = (EditText)findViewById(R.id.name);
+                String name = nameInput.getText().toString();
+
+                EditText quantityInput = (EditText)findViewById(R.id.quantity);
+                String quantity = quantityInput.getText().toString();
+
+                bag.add(name+" x"+quantity);
+                //The next line is needed in order to say to the ListView
+                //that the data has changed - we have added stuff now!
+                getMyAdapter().notifyDataSetChanged();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
